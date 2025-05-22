0c87c4ec1fa4d6ea342879dc5ef2d5bdfa122f9e
eleftheriaa <esolomonide@gmail.com>
2025-05-22 18:58:36 +0300

    SAC implementation

---
+++ sac/sac.py
@@ -0,0 +1,69 @@
+# this file implemetns the SAC algorithm
+
+import torch
+import torch.nn.functional as F
+import copy
+from LossesAndUpdates import update_actor, update_critic, update_temperature, soft_update
+from networks import Actor, Critic, Temperature
+
+class SAC(object):
+    def __init__(self, state_dim, action_dim, max_action, 
+                 discount=0.99, tau=0.005, actor_lr=3e-4, critic_lr=3e-4, alpha_lr=3e-4, target_entropy=None):
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+        # Actor network and optimizer
+        self.actor = Actor(state_dim, action_dim, max_action).to(self.device)
+        self.actor_optimizer = torch.optim.Adam(self.actor.parameters(), lr=actor_lr)
+
+        # Critic networks and target networks
+        self.critic = Critic(state_dim, action_dim).to(self.device)
+        self.critic_target = copy.deepcopy(self.critic)
+        self.critic_optimizer = torch.optim.Adam(self.critic.parameters(), lr=critic_lr)
+
+        # Temperature parameter (log_alpha is learnable)
+        self.log_alpha = torch.tensor(0.0, requires_grad=True, device=self.device)
+        self.alpha_optimizer = torch.optim.Adam([self.log_alpha], lr=alpha_lr)
+
+        # Entropy target
+        if target_entropy is None:
+            self.target_entropy = -action_dim  # Heuristic from SAC paper
+        else:
+            self.target_entropy = target_entropy
+
+        self.max_action = max_action
+        self.discount = discount
+        self.tau = tau
+        self.total_it = 0
+
+    def select_action(self, state):
+        state = torch.FloatTensor(state).unsqueeze(0).to(self.device)
+        return self.actor.select_action(state).cpu().numpy()[0]
+
+    def train(self, replay_buffer, batch_size=256):
+        self.total_it += 1
+
+        state, action, next_state, reward, not_done = replay_buffer.sample(batch_size)
+
+        # -------------------- Critic Update --------------------
+        update_critic(
+            self.critic, self.critic_target, self.critic_optimizer,
+            self.actor, self.alpha, state, action, reward, next_state, 1 - not_done,
+            self.discount
+        )
+
+        # -------------------- Actor Update --------------------
+        actor_loss, log_pi = update_actor(
+            self.actor, self.critic, self.actor_optimizer, self.alpha, state
+        )
+
+        # -------------------- Temperature Update --------------------
+        update_temperature(
+            self.alpha, self.alpha_optimizer, log_pi, self.target_entropy
+        )
+
+        # -------------------- Target Network Update --------------------
+        soft_update(self.critic, self.critic_target, self.tau)
+
+    def alpha(self):
+        # exponentiates the log α value.
+        return self.log_alpha.exp()
